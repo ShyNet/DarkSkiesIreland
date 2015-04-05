ab4cc923dfa3f73ce1da18a98cb2f3ce4175235e
antoinjudge <antoin21ok@gmail.com>
2015-04-05 18:29:18 +0100

    Map Added to Best Location Page

--- app/src/main/java/project/antoin/homepage1/BestLocationNow.java
+++ app/src/main/java/project/antoin/homepage1/BestLocationNow.java
@@ -3,12 +3,53 @@ package project.antoin.homepage1;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.*;
 
 
 public class BestLocationNow extends ActionBarActivity  {
+    GoogleMap googleMap;
+    private void createMapView(){
+        /**
+         * Catch the null pointer exception that
+         * may be thrown when initialising the map
+         */
+        try {
+            if(null == googleMap){
+                googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                        R.id.mapView)).getMap();
+
+                /**
+                 * If the map is still null after attempted initialisation,
+                 * show an error to the user
+                 */
+                if(null == googleMap) {
+                    Toast.makeText(getApplicationContext(),
+                            "Error creating map", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } catch (NullPointerException exception){
+            Log.e("mapApp", exception.toString());
+        }
+    }
+
+    private void addMarker(){
+
+        /** Make sure that the map has been initialised **/
+        if(null != googleMap){
+            googleMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(0, 0))
+                            .title("Marker")
+                            .draggable(true)
+            );
+        }
+    }
 
 
 
@@ -17,6 +58,8 @@ public class BestLocationNow extends ActionBarActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_best_location_now);
+        createMapView();
+        addMarker();
 
     }
 
